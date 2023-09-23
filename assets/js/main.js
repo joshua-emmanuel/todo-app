491fb66cd445db5bae6e9949b1e2d6f718da5886
Joshua Emmanuel <joshuaemmanuel925@gmail.com>
2023-09-23 21:06:48 +0100

    Add functionality for adding todos

---
+++ assets/js/main.js
@@ -0,0 +1,41 @@
+const todoApp = document.querySelector('#todo-app');
+const todoList = document.querySelector('.todo-list');
+
+function generateUniqueString(length) {
+  return Math.random().toString(36).substring(2, 2 + length);
+}
+
+function createTodo(todoName) {
+  const todoId = generateUniqueString(10);
+  const todo = document.createElement('li');
+  todo.classList.add('todo-list__item');
+  todo.innerHTML = DOMPurify.sanitize(`
+    <input type="checkbox" id="${todoId}">
+    <label class="todo-checkbox" for="${todoId}"></label>
+    <label for="${todoId}" class="todo-name">${todoName}</label>
+    <button type="button" class="remove-todo-btn">
+      <img src="./assets/images/icon-cross.svg" alt="">
+    </button>
+  `);
+
+  return todo;
+}
+
+function addTodo(event) {
+  event.preventDefault();
+
+  const todoInput = document.querySelector('#todo-input');
+  const todoInputValue = todoInput.value.trim();
+
+  todoInput.value = '';
+
+  if (todoInputValue === '') {
+    alert('Please enter a todo');
+    return;
+  }
+
+  const newTodo = createTodo(todoInputValue);
+  todoList.appendChild(newTodo);
+}
+
+todoApp.addEventListener('submit', addTodo);
